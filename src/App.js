49d52094d461d5b7f23834d1c942e399f6adf390
daniyal2339 <emailmeondaniyal@gmail.com>
2021-12-23 11:17:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
+import LoginForm from './Components/LoginForm';
+import './App.css';
+import ChatFeed from './Components/Chat';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-     mini projects
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
